c57121b12c8a1a8a7fb46962cb88d6d28625aaba
Dario <daminga2@utpl.edu.ec>
2023-11-25 20:20:56 -0500

    listo

---
+++ Solucion/Soluciones/src/soluciones/Ejercicio2.java
@@ -0,0 +1,15 @@
+package soluciones;
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        int numerador = 5;
+        int denominador = 10;
+        while(numerador <= 30){
+          System.out.println(numerador+ "/"+denominador);
+            numerador = numerador + 5;
+            denominador = denominador + 2 ;   
+        }
+           
+        
+    }
+ 
+}
