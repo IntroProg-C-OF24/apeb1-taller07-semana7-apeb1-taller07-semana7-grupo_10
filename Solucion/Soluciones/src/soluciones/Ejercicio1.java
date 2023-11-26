c491281f3f7071820f97b62c5017a05bc8d33b93
Dario <daminga2@utpl.edu.ec>
2023-11-25 20:03:30 -0500

    listo

---
+++ Solucion/Soluciones/src/soluciones/Ejercicio1.java
@@ -0,0 +1,16 @@
+package soluciones;
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int numerador;
+        int denominador;
+        numerador = 1;
+        denominador = 10;
+        while(numerador <= 6){
+            System.out.println(numerador+ "/" +denominador);
+            numerador++;
+            denominador++;
+           
+        }
+            
+    }
+}
